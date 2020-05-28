8cbda1c72aca232c1522921ba349392599b46b09
kalix123 <zeekalix@gmail.com>
2020-05-27 17:33:36 -0700

    add controller support

--- game/index.js
+++ game/index.js
@@ -2,3 +2,6 @@ $ = (queryString) => document.querySelector(queryString);
 function changeWallColor() {
   $('#background').setAttribute('color', 'blue');
 }
+$('#button').addEventListener('collide', function (evt) {
+  changeWallColor();
+});
